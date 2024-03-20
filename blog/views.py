745c5f60b5a4c0a820ba1ab3c91b6dee479907ea
Gabriel Salvador <gabosalvador2004@gmail.com>
2024-03-20 21:30:53 +0800

    moved templates to be stored app-level

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,14 @@
+from django.views.generic import DetailView, ListView
+
 from .models import Article
-from django.views.generic import ListView, DetailView
+
 
 # Create your views here.
 class ArticleDetailView(DetailView):
     model = Article
-    template_name = 'article_detail.html'
+    template_name = "blog/article_detail.html"
+
 
 class ArticleListView(ListView):
     model = Article
-    template_name = "article_list.html"
+    template_name = "blog/article_list.html"
