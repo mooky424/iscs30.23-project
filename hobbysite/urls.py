fa53de87f27209fc121bd8ab183bbee1215b83e5
Gabriel Salvador <gabosalvador2004@gmail.com>
2024-03-20 21:28:40 +0800

    fixed urls

--- hobbysite/urls.py
+++ hobbysite/urls.py
@@ -14,13 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('merchstore', include("merchstore.urls", namespace="merchstore")),
-    path('wiki', include("wiki.urls", namespace="wiki")),
-    path('commissions', include("commissions.urls", namespace="commissions")),
-    path('blog', include("blog.urls", namespace="blog")),
+    path("admin/", admin.site.urls),
+    path("merchstore/", include("merchstore.urls", namespace="merchstore")),
+    path("wiki/", include("wiki.urls", namespace="wiki")),
+    path("commissions/", include("commissions.urls", namespace="commissions")),
+    path("blog/", include("blog.urls", namespace="blog")),
 ]
