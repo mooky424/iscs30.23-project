00a0e9b0a53618b723262fc6adc4f8ae5c1a2d73
mrvpelagio <maria.pelagio@student.ateneo.edu>
2024-03-20 20:08:06 +0800

    changed templates

--- blog/admin.py
+++ blog/admin.py
@@ -3,6 +3,12 @@
 from django.contrib import admin
 from .models import ArticleCategory, Article
 
+class ArticleCategoryInline(admin.TabularInline):
+    model = ArticleCategory
+
+class ArticleInline(admin.TabularInline):
+    model = Article
+
 admin.site.register(ArticleCategory)
 admin.site.register(Article)
 
