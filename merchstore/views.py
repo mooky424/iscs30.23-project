345ec1e43847e7f5d85f354baac5f6cd361c8285
Gabriel Salvador <gabosalvador2004@gmail.com>
2024-04-26 17:24:10 +0800

    added new views

--- merchstore/views.py
+++ merchstore/views.py
@@ -1,15 +1,41 @@
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView
 from django.views.generic.list import ListView
 
-from .models import Product, ProductType
+from .models import Product, ProductType, Transaction
 
 
 class ProductListView(ListView):
-    model = ProductType
+    model = Product
     template_name = "merchstore/product_list.html"
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["product_types"] = ProductType.objects.all()
+        return context
+    
 
 
 class ProductDetailView(DetailView):
     model = Product
     template_name = "merchstore/product_detail.html"
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    template_name = "merchstore/product_create.html"
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    template_name = "merchstore/product_update.html"
+
+
+class CartView(ListView):
+    model = Transaction
+    template_name = "merchstore/cart.html"
+
+class TransactionListView(ListView):
+    model = Transaction
+    template_name = "merchstore/transaction_list.html"
