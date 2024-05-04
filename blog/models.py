6109a8b6c61c7a3c3e85c02cf92509e6a7ebf601
mrvpelagio <maria.pelagio@student.ateneo.edu>
2024-05-04 16:11:27 +0800

    edited models.py to fit specs, not done.

--- blog/models.py
+++ blog/models.py
@@ -1,31 +1,51 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User 
 
-class Article(models.Model): 
-    title = models.CharField(max_length = 255)
+
+class Article(models.Model):
+    author = models.ForeignKey(User, on_delete = models.SET_NULL)
+    title = models.CharField(max_length=255)
     entry_field = models.TextField()
-    created_on = models.DateTimeField(auto_now_add = True)
-    updated_on = models.DateTimeField(auto_now = True)
-    categories = models.ForeignKey("ArticleCategory", on_delete=models.SET_NULL, null=True, related_name = "articles")
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    categories = models.ForeignKey(
+        "ArticleCategory", on_delete=models.SET_NULL, null=True, related_name="articles"
+    )
 
     class Meta:
-        ordering = ['-created_on']
-        
+        ordering = ["-created_on"]
+
     def __str__(self):
         return self.title
-    
-    def get_absolute_url(self):
-        return reverse('blog:article_detail', args=[str(self.pk)])
 
-    
+    def get_absolute_url(self):
+        return reverse("blog:article_detail", args=[str(self.pk)])
+
+
 class ArticleCategory(models.Model):
-    name = models.CharField(max_length = 255)
+    name = models.CharField(max_length=255)
     description = models.TextField()
-    
+
     class Meta:
-        ordering = ['name']
+        ordering = ["name"]
         verbose_name_plural = "article categories"
 
-    def  __str__(self):
+    def __str__(self):
         return self.name
+    
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete = models.SET_NULL)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    entry_field = models.TextField()
+
+
+    class Meta: 
+        ordering = ["-created_on"]
+    
+    def __str__(self):
+        return self.title
+
+
 
