380b7f2736208e521aed8095805d8c09c1a1f701
mrvpelagio <maria.pelagio@student.ateneo.edu>
2024-05-06 16:41:29 +0800

    Updated urls, views, -- views for checking

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,10 @@ from .views import ArticleListView, ArticleDetailView
 urlpatterns = [
    path('articles/', ArticleListView.as_view(), name = 'blog'),
    path('article/<int:pk>', ArticleDetailView.as_view(), name = 'article_detail'),
-]
+   path('article/add/', ArticleCreateView.as_view(), name='article_create'),
+   path('article/<int:pk>/edit/', ArticleUpdateView.as_view(), name='article_update'),
+   path('article/create/', ArticleCreateView.as_view(), name='article_create_view'),
+   path('comment/create/<int:pk>/', CommentCreateView.as_view(), name='comment_create_view'), 
+] 
 
 app_name = "blog"
