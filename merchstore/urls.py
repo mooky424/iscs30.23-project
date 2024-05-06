890ecde727d8058a57ef1951bae2ac59ec310d01
Gabriel Salvador <gabosalvador2004@gmail.com>
2024-05-06 21:17:22 +0800

    productupdate view functionality

--- merchstore/urls.py
+++ merchstore/urls.py
@@ -6,7 +6,6 @@ from .views import (
     ProductDetailView,
     ProductListView,
     ProductUpdateView,
-    TransactionDetailView,
     TransactionListView,
 )
 
@@ -14,17 +13,12 @@ urlpatterns = [
     path("items", ProductListView.as_view(), name="product_list"),
     path("item/<int:pk>", ProductDetailView.as_view(), name="product_detail"),
     path("item/add", ProductCreateView.as_view(), name="product_create"),
-    path("item/<int:pk>", ProductUpdateView.as_view(), name="product_update"),
+    path("item/<int:pk>/edit", ProductUpdateView.as_view(), name="product_update"),
     path("cart", CartView.as_view(), name="cart"),
     path(
         "transactions",
         TransactionListView.as_view(),
         name="transaction_list",
-    ),
-    path(
-        "transaction/<int:pk>",
-        TransactionDetailView.as_view(),
-        name="transaction_detail",
     ),
 ]
 
--- merchstore/views.py
+++ merchstore/views.py
@@ -1,13 +1,13 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import redirect_to_login
 from django.shortcuts import redirect
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView
 from django.views.generic.list import ListView
-from django.contrib.auth.mixins import LoginRequiredMixin
 
 from user_management.models import Profile
 
-from .forms import TransactionForm
+from .forms import ProductForm, TransactionForm
 from .models import Product, ProductType, Transaction
 
 
@@ -59,11 +59,40 @@ class ProductDetailView(DetailView):
 class ProductCreateView(LoginRequiredMixin, CreateView):
     model = Product
     template_name = "merchstore/product_create.html"
+    form_class = ProductForm
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        form = context["form"]
+        form.fields["owner"].initial = Profile.objects.get(user=self.request.user)
+        form.fields["owner"].disabled = True
+        context["form"] = form
+        return context
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user.profile
+        return super().form_valid(form)
 
 
 class ProductUpdateView(LoginRequiredMixin, UpdateView):
     model = Product
-    template_name = "merchstore/product_update.html"
+    template_name = "merchstore/product_create.html"
+    form_class = ProductForm
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        form = context["form"]
+        form.fields["owner"].initial = Profile.objects.get(user=self.request.user)
+        form.fields["owner"].disabled = True
+        context["form"] = form
+        return context
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user.profile
+        product = self.get_object()
+        if form.is_valid() and product.stock == 0:
+            form.instance.status = "Out of Stock"
+        return super().form_valid(form)
 
 
 class CartView(LoginRequiredMixin, ListView):
@@ -72,7 +101,6 @@ class CartView(LoginRequiredMixin, ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["product_types"] = ProductType.objects.all()
         context["owners"] = Profile.objects.all()
         return context
 
@@ -81,7 +109,7 @@ class TransactionListView(LoginRequiredMixin, ListView):
     model = Transaction
     template_name = "merchstore/transaction_list.html"
 
-
-class TransactionDetailView(DetailView):
-    model = Transaction
-    template_name = "merchstore/transaction_detail.html"
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["buyers"] = Profile.objects.all()
+        return context
