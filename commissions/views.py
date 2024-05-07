f2ac6cf60244b6ebd8cd86a334c99e83e7227587
kormitw <roger.candari@student.ateneo.edu>
2024-05-07 17:13:54 +0800

    started updates on detail view

--- commissions/views.py
+++ commissions/views.py
@@ -3,7 +3,7 @@ from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-from .models import Commission
+from .models import Commission, Job, JobApplication
 
 
 class CommissionListView(ListView):
@@ -15,5 +15,17 @@ class CommissionDetailView(DetailView):
     model = Commission
     template_name = "commissions/commission_detail.html"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        for job_instance in Job.objects.all():
+            manpower_required = job_instance.manpower_required
+            accepted = JobApplication.objects.filter(status='Accepted',job=job_instance).count()
+            manpower_available = manpower_required - accepted
+
+        context["manpower_available"] = manpower_available
+        context["accepted"] = accepted
+        
+        return context
 
 # Create your views here.
