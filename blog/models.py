e5a27bc95032790fd009bf0cd821dc26192e8285
Gabriel Salvador <gabosalvador2004@gmail.com>
2024-05-08 22:01:14 +0800

    bug fixes

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,13 @@
 from django.contrib.auth.models import User
 from django.db import models
 from django.urls import reverse
+
 from user_management.models import Profile
 
+
 class ArticleCategory(models.Model):
-    name = models.CharField(max_length = 255)
-    description = models.TextField(null = True, blank = True)
+    name = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.title
@@ -15,31 +17,32 @@ class ArticleCategory(models.Model):
 
 
 class Article(models.Model):
-    title = models.CharField(max_length = 255)
+    title = models.CharField(max_length=255)
     author = models.ForeignKey(
         Profile,
-        on_delete = models.SET_NULL,
-        null = True,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="article_author",
     )
 
     category = models.ForeignKey(
-        ArticleCategory, 
-        on_delete = models.SET_NULL, 
-        null = True, 
-        related_name = "article_category"
-        )
+        ArticleCategory,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="article_category",
+    )
 
-    entry = models.TextField(null = True, blank = True)
-    header_image = models.ImageField(upload_to = "images/blog", null = True)
-    created_on = models.DateTimeField(null = False, editable = False, auto_now_add = True)
-    updated_on = models.DateTimeField(null = True, editable = False, auto_now = True)
-    
+    entry = models.TextField(null=True, blank=True)
+    header_image = models.ImageField(upload_to="images/blog", null=True)
+    created_on = models.DateTimeField(null=False, editable=False, auto_now_add=True)
+    updated_on = models.DateTimeField(null=True, editable=False, auto_now=True)
+
     def __str__(self):
         return self.title
-    
+
     def get_absolute_url(self):
-        return reverse('blog:article_detail', args = [self.pk])
-    
+        return reverse("blog:article_detail", args=[self.pk])
+
     class Meta:
         ordering = ["-updated_on"]
 
@@ -47,26 +50,26 @@ class Article(models.Model):
 class Comment(models.Model):
     author = models.ForeignKey(
         Profile,
-        on_delete = models.SET_NULL,
-        null = True,
-        blank = True,
-        related_name = 'comment_author'
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="article_comment_author",
     )
 
     article = models.ForeignKey(
         Article,
-        on_delete = models.CASCADE,
-        null = True,
-        blank = True,
-        related_name = 'comment_article'
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="article_comment",
     )
 
     entry = models.TextField(null=True)
-    created_on = models.DateTimeField(null = False, auto_now_add = True)
-    updated_on = models.DateTimeField(null = True, auto_now = True)
+    created_on = models.DateTimeField(null=False, auto_now_add=True)
+    updated_on = models.DateTimeField(null=True, auto_now=True)
 
     def __str__(self):
         return f"Comment by {self.author} on {self.article.title}"
-    
+
     class Meta:
         ordering = ["-created_on"]
