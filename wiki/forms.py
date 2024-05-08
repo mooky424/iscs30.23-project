ee84e655758137dab3bff9513134de4919efe848
Czyrone <czyrone.librea@student.ateneo.edu>
2024-05-08 13:22:07 +0800

    Fixing profile issues

---
+++ wiki/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from .models import Article, ArticleCategory, Comment
+
+class ArticleForm(forms.ModelForm):
+    class Meta:
+        model = Article
+        fields = ('title', 'category', 'entry', 'header_image')
+        widgets = {
+            "category" : forms.Select()
+        }
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ["entry"]
+        widgets = {
+            "entry" : forms.Textarea()
+        }
