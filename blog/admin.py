7333b312b1d7667ca654b56b268ffc507a53ac9e
mrvpelagio <maria.pelagio@student.ateneo.edu>
2024-05-08 12:08:16 +0800

    modified admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,15 +1,34 @@
-# Register your models here.
-
 from django.contrib import admin
-from .models import ArticleCategory, Article
+from .models import ArticleCategory, Article, Comment
+
+
+class CommentInLine(admin.TabularInline):
+    model = Comment
+
+class ArticleInLine(admin.TabularInline):
+    model = Article
 
 class ArticleCategoryAdmin(admin.ModelAdmin):
     model = ArticleCategory
+    inlines = [ArticleInLine]
 
 class ArticleAdmin(admin.ModelAdmin):
     model = Article
+    inlines = [CommentInLine]
 
-admin.site.register(Article, ArticleAdmin)
+    search_fields = [
+        "title",
+    ]
+
+    list_display = ["title", "author", "category"]
+
+    list_filter = [
+        "created_on",
+        "updated_on",
+    ]
+
+    fields = ["title", "author", "category", "entry","header-image"]
+
+
 admin.site.register(ArticleCategory, ArticleCategoryAdmin)
-
-
+admin.site.register(Article, ArticleAdmin)
