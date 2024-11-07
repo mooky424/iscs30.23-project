f988dad9b2886e614a8aaa783f4900ab88b83f81
Gabriel Salvador <gabosalvador2004@gmail.com>
2024-11-07 18:45:42 +0800

    made product list page as homepage

--- hobbysite/urls.py
+++ hobbysite/urls.py
@@ -22,7 +22,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("merchstore/", include("merchstore.urls", namespace="merchstore")),
+    path("", include("merchstore.urls", namespace="merchstore")),
     path("wiki/", include("wiki.urls", namespace="wiki")),
     path("commissions/", include("commissions.urls", namespace="commissions")),
     path("blog/", include("blog.urls", namespace="blog")),
--- merchstore/urls.py
+++ merchstore/urls.py
@@ -10,7 +10,7 @@ from .views import (
 )
 
 urlpatterns = [
-    path("items", ProductListView.as_view(), name="product_list"),
+    path("", ProductListView.as_view(), name="product_list"),
     path("item/<int:pk>", ProductDetailView.as_view(), name="product_detail"),
     path("item/add", ProductCreateView.as_view(), name="product_create"),
     path("item/<int:pk>/edit", ProductUpdateView.as_view(), name="product_update"),
