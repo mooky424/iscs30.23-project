e54eab1282dc3b81885d14d772654d41b7cf21a7
Gabriel Salvador <gabosalvador2004@gmail.com>
2024-12-07 18:57:30 +0800

    fix entrypoint

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 APP_PORT=${PORT:-8000}
+SUPERUSER_EMAIL=${DJANGO_SUPERUSER_EMAIL:-"admin@example.com"}
 cd /hobbysite/
+/opt/venv/bin/python manage.py collectstatic
+/opt/venv/bin/python manage.py migrate --noinput
+/opt/venv/bin/python manage.py createsuperuser --email $SUPERUSER_EMAIL --noinput || true
 /opt/venv/bin/gunicorn --worker-tmp-dir /dev/shm hobbysite.wsgi:application --bind "0.0.0.0:${APP_PORT}"
--- web/migrate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-SUPERUSER_EMAIL=${DJANGO_SUPERUSER_EMAIL:-"admin@example.com"}
-cd /hobbysite/
-
-/opt/venv/bin/python manage.py collectstatic
-/opt/venv/bin/python manage.py migrate --noinput
-/opt/venv/bin/python manage.py createsuperuser --email $SUPERUSER_EMAIL --noinput || true
